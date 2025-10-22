4768288eb42daa264abaaf17d87e2622aa81e5da
Maria Achim <0achic44@solent.ac.uk>
2025-10-22 15:25:42 +0100

    Changed walk function

--- src/Cat.kt
+++ src/Cat.kt
@@ -3,9 +3,9 @@ class Cat (val name: String, var age: Int, var weight: Int) {
     // Reduces weight by one
     // Should check if the weight is 5 or more
     // There is a bug - don't resolve until instructed by the exercise.
-    fun walk() :Boolean {
+    fun walk(distance: Int) :Boolean {
         if(weight <= 5) {
-            weight--
+            weight = weight - distance
             return true
         }
         return false
